0f048e09903e3525c8fa6cb22e70d93feb201cd0
jimmy <jimmyvoc@gmail.com>
2023-01-30 21:33:40 -0500

    se agrega el proyecto

--- final.py
+++ final.py
@@ -7,8 +7,7 @@ from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
 import plotly.express as px
 from dash import no_update
-from flask import Flask
-from gevent.pywsgi import WSGIServer
+
 
 # Create a dash application
 app = dash.Dash(__name__)
